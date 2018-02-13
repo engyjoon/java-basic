d2d2a380231d15f2c81de8cea811f5582974183e
Yong Joon Kwon <bly99@naver.com>
2018-02-13 16:33:07 +0900

    조건식 반복시 완료

---
+++ java-basic/src/Flow12.java
@@ -0,0 +1,18 @@
+
+public class Flow12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for (int i=1; i<=5; i++) {
+			System.out.println(i);
+		}
+		
+		for (int i=1; i<=5; i++) {
+			System.out.print(i);
+		}
+		
+		System.out.println();
+	}
+
+}
--- java-basic/src/OperatorEx24.java
+++ java-basic/src/OperatorEx24.java
@@ -11,7 +11,7 @@ public class OperatorEx24 {
 		System.out.printf("x = %2d, 10 < x && x < 20 ==>> %b %n", x, 10 < x && x < 20);
 		
 		x = 6;
-		System.out.printf("x = %2d, x %% 2 == 0 || x %% 3 == 0 && x %% 6 != 0 ==>> %b %n", 
+		System.out.printf("x = %2d, x %% 2 == 0 || x %% 3 == 0 && x %% 6 != 0   ==>> %b %n", 
 				x, x % 2 == 0 || x % 3 == 0 && x%6 != 0);
 		System.out.printf("x = %2d, (x %% 2 == 0 || x %% 3 == 0) && x %% 6 != 0 ==>> %b %n", 
 				x, (x % 2 == 0 || x % 3 == 0) && x%6 != 0);
@@ -21,6 +21,12 @@ public class OperatorEx24 {
 		
 		ch = 'a';
 		System.out.printf("ch = '%c', 'a' <= ch && ch <= 'z' ==>> %b %n", ch, 'a' <= ch && ch <= 'z');
+		
+		ch = 'A';
+		System.out.printf("ch = '%c', 'A' <= ch && ch <= 'Z' ==>> %b %n", ch, 'A' <= ch && ch <= 'Z');
+		
+		ch = 'q';
+		System.out.printf("ch = '%c', 'q' == ch && ch == 'Q' ==>> %b %n", ch, 'q' == ch || ch == 'Q');
 	}
 
 }
